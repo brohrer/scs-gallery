a324cfe38d8d99f5aa8c909411902276f0d98d79
brohrer <brohrer@gmail.com>
2022-05-22 14:05:46 -0400

    edits

--- log_y_histograms.py
+++ log_y_histograms.py
@@ -5,41 +5,20 @@ import matplotlib.patches as patches
 from matplotlib.ticker import FixedLocator, FixedFormatter
 
 
-# filename = "data/params/scs_weight_01.pkl"
-filename = "data/params/scs_p_01.pkl"
-# filename = "data/params/scs_out_01.pkl"
+def log_histogram(data, name):
+    """
+    Create ridgeline plots, where each individual plot is a
+    log-y scaled histogram.
 
-def main():
-    with open(filename, "rb") as f:
-        scs_out = pkl.load(f)
-        n_vals_per_iter = 0
-        n_iters = 0
-        n_epochs = 0
-        out_vals = {}
-        for name, vals in scs_out.items():
-            n_vals_per_iter = vals.size
-            layer, i_epoch, i_iter = name.split("_")
-            n_iters = np.maximum(n_iters, int(i_iter) + 1)
-            n_epochs = np.maximum(n_epochs, int(i_epoch) + 1)
-            out_vals[layer] = None
+    data: 2D Numpy array
+    Each row contains all the values to include in a single histogram.
+    0th row will be in the lowest ridgeline plot.
+    Every Nth row will be a separate histogram. N is selected according
+    to n_target_rows, below to avoid overwhelming the viewer.
 
-        for layer in out_vals.keys():
-            out_vals[layer] = np.zeros((n_epochs, n_iters * n_vals_per_iter))
-
-        for name, vals in scs_out.items():
-            layer, i_epoch, i_iter = name.split("_")
-            i_iter = int(i_iter)
-            i_epoch = int(i_epoch)
-            out_vals[layer][
-                    i_epoch,
-                    n_vals_per_iter * i_iter: n_vals_per_iter * (i_iter + 1)
-                    ] = vals
-
-    for layer, vals in out_vals.items():
-        histogram(vals, layer)
-
-
-def histogram(data, name):
+    name: str
+    A name stem for the results image.
+    """
     dpi = 300
     border = .125
     eps = 1e-6
@@ -95,8 +74,46 @@ def histogram(data, name):
     ax.yaxis.set_major_formatter(y_formatter)
     ax.yaxis.set_major_locator(y_locator)
 
-    plt.savefig("vis/hist_" + name + ".png", dpi=dpi)
+    plt.savefig("hist_" + name + ".png", dpi=dpi)
     plt.close()
+
+
+# This code is specific to my use case.
+# It parses the values I pulled out and stashed in a dict with a
+# key naming scheme that lets me re-create a Numpy array from it.
+
+# filename = "data/params/scs_weight_01.pkl"
+filename = "data/params/scs_p_01.pkl"
+# filename = "data/params/scs_out_01.pkl"
+
+def main():
+    with open(filename, "rb") as f:
+        scs_out = pkl.load(f)
+        n_vals_per_iter = 0
+        n_iters = 0
+        n_epochs = 0
+        out_vals = {}
+        for name, vals in scs_out.items():
+            n_vals_per_iter = vals.size
+            layer, i_epoch, i_iter = name.split("_")
+            n_iters = np.maximum(n_iters, int(i_iter) + 1)
+            n_epochs = np.maximum(n_epochs, int(i_epoch) + 1)
+            out_vals[layer] = None
+
+        for layer in out_vals.keys():
+            out_vals[layer] = np.zeros((n_epochs, n_iters * n_vals_per_iter))
+
+        for name, vals in scs_out.items():
+            layer, i_epoch, i_iter = name.split("_")
+            i_iter = int(i_iter)
+            i_epoch = int(i_epoch)
+            out_vals[layer][
+                    i_epoch,
+                    n_vals_per_iter * i_iter: n_vals_per_iter * (i_iter + 1)
+                    ] = vals
+
+    for layer, vals in out_vals.items():
+        log_histogram(vals, layer)
 
 
 if __name__ == "__main__":
